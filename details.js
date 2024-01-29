d0cbd7dd1f7de28472247c4e558db9955e818917
MohamadGhnemat <aohamad.ahmad9696@gmail.com>
2024-01-29 02:49:47 +0200

    using async await with axios

--- details.js
+++ details.js
@@ -1,30 +1,26 @@
-function getProductInfo(){
+
+async function getProductInfo() {
+  try {
     let searchParams = new URLSearchParams(window.location.search);
- 
+
     var params = searchParams.get('productID');
-    // console.log(params);
-  
-    axios.get(`https://dummyjson.com/products/${params}`)
-  .then(function (response) {
-   const data = response.data;
-   const {title,thumbnail,category,brand,rating,description,price,discountPercentage} = data
-   
-  
+    const response = await axios.get(`https://dummyjson.com/products/${params}`);
+    const data = response.data;
+ const {title,thumbnail,category,brand,rating,description,price,discountPercentage} = data
+ 
 
-     document.querySelector('.title').innerHTML = title;
-     document.querySelector('.category').innerHTML = category;
-     document.querySelector('.price').innerHTML = price;
-     document.querySelector('.image').src = thumbnail;
 
-     document.querySelector('.rating').innerHTML = rating;
-     document.querySelector('.brand').innerHTML = brand;
-     document.querySelector('.description').innerHTML = description;
-   
-    console.log(response);
-  })
-  .catch(function (error) {
+   document.querySelector('.title').innerHTML = title;
+   document.querySelector('.category').innerHTML = category;
+   document.querySelector('.price').innerHTML = price;
+   document.querySelector('.image').src = thumbnail;
 
-    console.log(error);
-  })
+   document.querySelector('.rating').innerHTML = rating;
+   document.querySelector('.brand').innerHTML = brand;
+   document.querySelector('.description').innerHTML = description;
+  } catch (error) {
+    console.error(error);
+  }
 }
+
 getProductInfo()
